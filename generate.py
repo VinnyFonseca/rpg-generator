e8619b8d1939c04f94fee7487af613c7e52fa2bf
Vinny Fonseca <vinny.fonseca@codemdigital.com>
2021-03-29 00:36:37 +0100

    Lint, improved behaviour and result tables

--- generate.py
+++ generate.py
@@ -1,47 +1,59 @@
+# Required installation
+# pip install prettytable
+
 # Imports
 from datetime import datetime
 import json
 import os
 import random
-from tabulate import tabulate
+import sys
+from prettytable import PrettyTable
 
 # User choices
 while True:
-    choice = input ("What would you like to generate?\n[1] Traps\n[2] Treasures\n[q] quit\nOption: ")
-    if choice == "1":
-        feed_type = 'traps'
-        break
-    elif choice == "2":
-        feed_type = 'treasures'
-        break
-    elif choice == "q":
-        break
+    choice = input('''What would you like to generate?
 
-generation_amount = int(input("How many? -> "))
+[1] Traps
+[2] Treasures
+[q] quit
 
-# Read chose feed
-with open('data/{}.json'.format(feed_type), 'r') as myfile:
-    data = myfile.read()
+Choose -> ''')
 
-feed = json.loads(data)
+    if choice == '1':
+        FEED_TYPE = 'traps'
+        break
+    if choice == '2':
+        FEED_TYPE = 'treasures'
+        break
+    if choice == 'q':
+        sys.exit()
+
+generation_amount = int(input('How many? -> '))
+
+# Read chosen feed
+with open('data/{}.json'.format(FEED_TYPE), 'r') as myfile:
+    FEED_FILE = json.loads(myfile.read())
+
 
 # Single entry generation
+def get_die_type(rolls):
+    last_roll = list(rolls.keys())[-1].split('-')
+    highest_roll = last_roll[1] if len(last_roll) > 1 else last_roll[0]
+
+    return int(highest_roll) if highest_roll.isnumeric() else 6
+
+
+# Random die roll based on autodetected number of faces
+def roll_die(faces):
+    return random.randint(1, faces)
+
+
+# Main function
 def generate_entry():
     headers = []
     entry = []
 
-    def get_die_type(rolls):
-        last_roll = list(rolls.keys())[-1].split('-')
-        highest_roll = last_roll[1] if len(last_roll) > 1 else last_roll[0]
-
-        return int(highest_roll) if highest_roll.isnumeric() else 6
-
-    def roll_die(faces):
-        return random.randint(1, faces)
-
-    def save_attribute(attribute):
-        entry.append(attribute)
-
+    # Recursive function
     def get_attribute(header, attribute, die):
         is_rollable = '-' in header or header.isnumeric()
 
@@ -54,37 +66,51 @@ def generate_entry():
                 for key, value in attribute.items():
                     get_attribute(key, value, roll_result)
             else:
-                save_attribute(attribute)
+                entry.append(attribute)
         else:
             rollable_range = is_rollable and header.split('-')
-            matches_roll_value = rollable_range and (die == int(rollable_range[0]) or (len(rollable_range) > 1 and int(rollable_range[0]) <= die <= int(rollable_range[1])))
+            matches_roll_value = rollable_range and (
+                die == int(rollable_range[0]) or (
+                    len(rollable_range) > 1
+                    and int(rollable_range[0]) <= die <= int(rollable_range[1])
+                ))
 
             if matches_roll_value:
-                if type(attribute) is dict:
+                if isinstance(attribute, dict):
                     die = get_die_type(attribute)
                     roll_result = roll_die(die)
 
                     for key, value in attribute.items():
                         get_attribute(key, value, roll_result)
                 else:
-                    save_attribute(attribute)
+                    entry.append(attribute)
 
-    for header, attribute in feed.items():
+    # Init recursion
+    for header, attribute in FEED_FILE.items():
         roll_result = roll_die(6)
         get_attribute(header, attribute, roll_result)
 
-    return tabulate([entry], headers=headers)
+    # Generate entry table
+    pt = PrettyTable()
+    pt.field_names = headers
+    pt.add_row(entry)
+    return str(pt)
+
 
 # Final list generation
 def generate():
-    f = open('results/{}_{}.txt'.format(feed_type, datetime.now().strftime("%Y-%m-%d_%H:%M:%S")), 'a')
-    i = 0
+    file = open('results/{}_{}.txt'.format(
+        FEED_TYPE,
+        datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    ), 'a')
+    counter = 0
 
-    while i < generation_amount:
-        f.write(generate_entry() + '\n\n')
-        i += 1
+    while counter < generation_amount:
+        file.write(generate_entry() + '\n')
+        counter += 1
 
-    f.close()
+    file.close()
+
 
 # Write to disk
 if not os.path.exists('results'):
