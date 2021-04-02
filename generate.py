db2a3a81afd462032c221ea245d773b990b0568b
binn05 <binn05@gmail.com>
2021-04-02 16:56:23 -0300

    mudança formato de codificação de texto para iso-8859-1

--- generate.py
+++ generate.py
@@ -41,7 +41,7 @@ def read_feed(name, partial=False):
     with open((
         './data/{}.json' if not partial
         else './data/partials/{}'
-    ).format(name), 'r', encoding='utf-8') as file:
+    ).format(name), 'r', encoding='iso-8859-1') as file:
         return json.loads(file.read())
 
 
